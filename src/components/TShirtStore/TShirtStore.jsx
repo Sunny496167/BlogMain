106fee9329ccaaf56b942baed71e776e68eb4a1d
Sunny Kumar <sunnykumar49617@gmail.com>
2024-08-29 18:05:22 +0530

    little bit change

--- src/components/TShirtStore/TShirtStore.jsx
+++ src/components/TShirtStore/TShirtStore.jsx
@@ -4,7 +4,7 @@ const ProductCard = ({ product }) => {
   const [selectedSize, setSelectedSize] = useState("Regular");
 
   return (
-    <div className="max-w-4xl mx-auto p-4 mt-4 mb-4 flex flex-col md:flex-row bg-white shadow-md rounded-lg">
+    <div className="max-w-4xl mx-auto p-4 mt-4 mb-4 flex flex-col md:flex-row bg-white shadow-[4px_4px_20px_rgba(0,0,0,0.25)] rounded-lg">
       <div className="flex flex-col mr-4">
         {product.images.map((image, index) => (
           <img
